de84cdee5769398c1538c1348ed4bf951184bb4d
V1dar2r <dnenghksw@naver.com>
2026-09-17 22:33:00 +0900

    feat: 글 조회 기능 추가

--- src/main/java/project/board/Controller/PostController.java
+++ src/main/java/project/board/Controller/PostController.java
@@ -1,14 +1,13 @@
 package project.board.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.board.Post.Post;
 import project.board.Post.PostCreateRequest;
 import project.board.Post.PostRecord;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,5 +22,15 @@ public class PostController {
         store.put(id,result);
         return result;
     }
+    @GetMapping("/posts")
+    public List<Post> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    @GetMapping("/posts/{id}")
+    public Post findOne(@PathVariable Long id){
+        return store.get(id);
+    }
+
 
 }
